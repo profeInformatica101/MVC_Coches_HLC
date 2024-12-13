e3879232ea719a640f25a84b36286bad035b68ee
Juan <jdommay863@g.educaand.es>
2024-12-13 09:22:29 +0100

    inicio de proyecto

---
+++ coche_mvc/src/main/java/com/hlc/coche_mvc/CocheMvcApplication.java
@@ -0,0 +1,13 @@
+package com.hlc.coche_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocheMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocheMvcApplication.class, args);
+	}
+
+}
---
+++ coche_mvc/src/test/java/com/hlc/coche_mvc/CocheMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlc.coche_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocheMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
